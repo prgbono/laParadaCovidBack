2fa52861c790e8af6b5aea269a27ccce253acf04
prgbono <pacoriosgalan@gmail.com>
2020-11-10 20:09:19 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3968fb266b1f36a98bad70f2bfddb765'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '74499889-85dd-4903-82c9-8c2a0eddb4fd'
+};
